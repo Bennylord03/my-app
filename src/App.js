1a1d51a8f232516fb4513a9f4a1d0b92a0db9506
Bennylord03 <hyitishreg@gmail.com>
2023-08-05 14:19:13 +0800

    create structure

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="grid grid-rows-1 grid-flow-col gap-4 w-full bg-red-300">
+        <div className="bg-blue-300 flex">
+          <div className="bg-green-300 flex-1 w-33">
+            1
+          </div>
+          <div className="bg-yellow-300 flex-1 w-15">
+            2
+          </div>
+        </div>
+
+        <div className="bg-blue-300">
+          09
+        </div>
+      </div>
       </header>
     </div>
   );
